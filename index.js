ca4a9c04b8c9306f0f251d07cd3d4d67d05ec77b
Diegocarrero <diegocar2413@gmail.com>
2023-03-07 21:32:14 -0500

    Update file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function fn(...c) {
-  if (!c.every((a) => Number.isNumber(a)))
+function sumAllNumbers(...elements) {
+  if (!elements.every((element) => Number.isNumber(element)))
     throw "All arguments must be numbers.";
-  return c.reduce((a, b) => a + b);
+  return elements.reduce((a, b) => a + b);
 }
 
-fn(1, 2, 3); //> 6
-fn(10, "B", 20); //> error All arguments must be numbers.
+sumAllNumbers(1, 2, 3); //> 6
+sumAllNumbers(10, "B", 20); //> error All arguments must be numbers.
