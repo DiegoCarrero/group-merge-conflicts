4bce224e2323ff857fa8f28ac37dbded307a1675
Angel <EMAIL>
2023-03-07 21:28:33 -0500

    angels changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function fn(...c) {
-  if (!c.every((a) => Number.isNumber(a)))
+function fn(...count) {
+  if (!count.every((a) => Number.isNumber(a)))
     throw "All arguments must be numbers.";
-  return c.reduce((a, b) => a + b);
+  return count.reduce((a, b) => a + b);
 }
 
-fn(1, 2, 3); //> 6
-fn(10, "B", 20); //> error All arguments must be numbers.
+fn(1, 2, 2); //> 6
+fn(10, 100, 20); //> error All arguments must be numbers.
